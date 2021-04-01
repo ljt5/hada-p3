01b2df412f18f4e7a7409f0105d894abea856d5d
Luis Jiménez Tortosa <ljt5@alu.ua.es>
2021-04-01 19:10:30 +0200

    74011863W Añade el proyecto library
    Incluye las clases CADUsuario y ENUsuario con sus atributos y métodos aún no implementados.

---
+++ library/CADUsuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class CADUsuario
+    {
+        private string constring;
+
+        public CADUsuario()
+        {
+        }
+        public bool createUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readFirstUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readNextUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readPrevUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool updateUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool deleteUsuario(ENUsuario en)
+        {
+            return false;
+        }
+    }
+}
---
+++ library/ENUsuario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class ENUsuario
+    {
+        private string nif;
+        private string nombre;
+        private int edad;
+
+        public ENUsuario()
+        {
+        }
+        public ENUsuario(string nif, string nombre, int edad)
+        {
+        }
+        public bool createUsuario()
+        {
+            return false;
+        }
+        public bool readUsuario()
+        {
+            return false;
+        }
+        public bool readFirstUsuario()
+        {
+            return false;
+        }
+        public bool readNextUsuario()
+        {
+            return false;
+        }
+        public bool readPrevUsuario()
+        {
+            return false;
+        }
+        public bool updateUsuario()
+        {
+            return false;
+        }
+        public bool deleteUsuario()
+        {
+            return false;
+        }
+    }
+}
