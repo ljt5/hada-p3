dc9dca31565eda29ec086548d87e2675f5645a00
Luis Jiménez Tortosa <ljt5@alu.ua.es>
2021-04-01 14:22:09 +0200

    74011863W Crea el proyecto usuWeb
    Añade a la solución en blanco el proyecto usuWeb. Crea el formulario web con página maestra y las cajas de texto y botones.

---
+++ usuWeb/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="usuWeb.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Página de usuarios</h1>
+    <p>
+        NIF:
+        <asp:TextBox ID="nifTextBox" TextMode="SingleLine"
+            Columns="30" runat="server" />
+    </p>
+    <p>
+        Nombre:
+        <asp:TextBox ID="nombreTextBox"
+            TextMode="Password" Columns="30" runat="server" />
+    </p>
+    <p>
+        Edad:
+        <asp:TextBox ID="edadTextBox"
+            TextMode="Password" Columns="30" runat="server" />
+    </p>
+    <p>
+        <asp:Button id="BotonLeer" Text="Leer" runat="server"/>
+        <asp:Button id="BotonLeerPrimero" Text="Leer Primero" runat="server"/>
+        <asp:Button id="BotonLeerAnterior" Text="Leer Anterior" runat="server"/>
+        <asp:Button id="BotonLeerSiguiente" Text="Leer Siguiente" runat="server"/>
+        <asp:Button id="BotonCrear" Text="Crear" runat="server"/>
+        <asp:Button id="BotonActualizar" Text="Actualizar" runat="server"/>
+        <asp:Button id="BotonBorrar" Text="Borrar" runat="server"/>
+    </p>
+</asp:Content>
---
+++ usuWeb/Default.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace usuWeb
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control nifTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nifTextBox;
+
+        /// <summary>
+        /// Control nombreTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nombreTextBox;
+
+        /// <summary>
+        /// Control edadTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox edadTextBox;
+
+        /// <summary>
+        /// Control BotonLeer.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeer;
+
+        /// <summary>
+        /// Control BotonLeerPrimero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerPrimero;
+
+        /// <summary>
+        /// Control BotonLeerAnterior.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerAnterior;
+
+        /// <summary>
+        /// Control BotonLeerSiguiente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerSiguiente;
+
+        /// <summary>
+        /// Control BotonCrear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCrear;
+
+        /// <summary>
+        /// Control BotonActualizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonActualizar;
+
+        /// <summary>
+        /// Control BotonBorrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonBorrar;
+    }
+}
